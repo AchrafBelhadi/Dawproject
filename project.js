432f487302836904dd50c8f4b82d084ccbaef679
AchrafBelhadi <achraf.belhadi@etu.univ-batna2.dz>
2024-05-03 22:53:33 +0100

    daw project

---
+++ project.js
@@ -0,0 +1,15 @@
+var name = prompt("Please enter your Name and Surname",".");
+var gender = prompt("Please enter your gender","male");
+
+if (gender == 'male')
+{
+    alert("Welcome, MR. " + name);
+
+}
+else if (gender == 'female')
+{
+    alert("Welcome, MS. " + name);
+
+} else {
+    alert("Welcome, Unknown gender ("+ gender +") " + name);
+}
